6c5de2aad00426b9b36d442e4d0c3c068ffbf8cf
jared Long <jlong4223@gmail.com>
2020-10-26 10:54:32 -0500

    changed API and text values

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 // alert("this is js speaking")
 
-//Beer API
-// https://sandbox-api.brewerydb.com/v2/beer/random/?key=203519b93825fa67c097215a92281dc0
+/* This was my first API that I tried to connect to but kept recieving CORS error
+Beer API
+https://sandbox-api.brewerydb.com/v2/beer/random/?key=203519b93825fa67c097215a92281dc0
 
-//https://sandbox-api.brewerydb.com/v2/{endpoint}?key=203519b93825fa67c097215a92281dc0
+https://sandbox-api.brewerydb.com/v2/{endpoint}?key=203519b93825fa67c097215a92281dc0
+
+Search beers by name: 
+`https://sandbox-api.brewerydb.com/v2/beers/?name=freedom+Tickler&key=203519b93825fa67c097215a92281dc0`
+
+`https://sandbox-api.brewerydb.com/v2/beer/${searchText}/?key=203519b93825fa67c097215a92281dc0`
+
+`https://sandbox-api.brewerydb.com/v2/beers/?name=%27murican+Pilsner&key=203519b93825fa67c097215a92281dc0`
+
+search beers by id:
+https://sandbox-api.brewerydb.com/v2/beer/eVgnFV/?key=203519b93825fa67c097215a92281dc0
+*/
+
+/* API number 2
 
 
-//search beers by id:
-//https://sandbox-api.brewerydb.com/v2/beer/eVgnFV/?key=203519b93825fa67c097215a92281dc0
+*/
+
 
 
 console.log("its working!")
@@ -17,17 +31,18 @@ function handleGetData(event) {
     const searchText = $("#search").val()
     $.ajax({
         url: 
-        `https://sandbox-api.brewerydb.com/v2/beer/${searchText}/?key=203519b93825fa67c097215a92281dc0`
+            `https://api.punkapi.com/v2/beers/?beer_name=choco+Libre`
     }).then(
         (data) => {
             console.log(data);
-            $("#beername").text(data.name)
-            $("#abv").text(data.abv)
-            $("#description").text(data.style.description)
-            $("#created").text(data.createDate)
+            $("#beername").text(data[0].name)
+            $("#abv").text(data[0].abv + '%')
+            $("#description").text(data[0].description)
+            $("#pairing").text(data[0].food_pairing)
         },
         (error) => {
-            console.log("bad request: ", error)
+            console.log("bad request: ", error), 
+            alert("bad request")
         } 
     )
 }
