d94b623613f887bd1a534d6d01a8b811b4e5a06d
jared Long <jlong4223@gmail.com>
2020-10-28 10:36:49 -0500

    deleted repetitive code and old comments

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 // alert("this is js speaking")
 
-/* This was my first API that I tried to connect to but kept recieving CORS error
-Beer API
-https://sandbox-api.brewerydb.com/v2/beer/random/?key=203519b93825fa67c097215a92281dc0
-
-https://sandbox-api.brewerydb.com/v2/{endpoint}?key=203519b93825fa67c097215a92281dc0
-
-Search beers by name: 
-`https://sandbox-api.brewerydb.com/v2/beers/?name=freedom+Tickler&key=203519b93825fa67c097215a92281dc0`
-
-`https://sandbox-api.brewerydb.com/v2/beer/${searchText}/?key=203519b93825fa67c097215a92281dc0`
-
-
 /* API number 2
     //with beer name that I used to test data in the console
 https://api.punkapi.com/v2/beers/?beer_name=choco+Libre
